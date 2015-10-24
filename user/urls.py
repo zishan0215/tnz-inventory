1ae711580a7d333389d220328c1a1c930cb1715c
Zishan Ahmad <zishanahmad@outlook.com>
2015-10-25 01:25:36 +0530

    added all the required pages

--- user/urls.py
+++ user/urls.py
@@ -5,5 +5,8 @@ urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^login/$', views.login_user, name='login'),
     url(r'^dashboard/', views.dashboard, name='dashboard'),
+    url(r'^new/', views.new_item, name='new_item'),
+    url(r'^view/', views.view_items, name='view_items'),
+    url(r'^search/', views.search, name='search'),
     url(r'^logout/', views.logout_user, name='logout'),
 )
